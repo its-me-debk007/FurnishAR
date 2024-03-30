cf7f09dd85eb5ac5201f2cb0cf5c5ff59373c3bd
Debashish Kundu <debashish.joy@gmail.com>
2024-03-30 17:11:59 +0530

    only image assets

--- app/src/main/java/in/furniture/furnishar/ui/theme/Color.kt
+++
@@ -1,7 +0,0 @@
-package `in`.furniture.furnishar.ui.theme
-
-import androidx.compose.ui.graphics.Color
-
-val ColorPrimary = Color(0xFF8A49F7)
-val ColorSecondaryText = Color(0x80000000)
-val WarningBackgroundColor = Color(0xFFfff8e6)
