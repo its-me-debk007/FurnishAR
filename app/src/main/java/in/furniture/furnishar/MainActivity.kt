cf37c16340b0fec4305c4426d3a37892be208544
Debashish Kundu <debashish.joy@gmail.com>
2024-05-15 19:54:48 +0530

    integrate razorpay sdk

--- app/src/main/java/in/furniture/furnishar/MainActivity.kt
+++ app/src/main/java/in/furniture/furnishar/MainActivity.kt
@@ -1,6 +1,7 @@
 package `in`.furniture.furnishar
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -11,16 +12,52 @@ import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.razorpay.Checkout
+import com.razorpay.ExternalWalletListener
+import com.razorpay.PaymentData
+import com.razorpay.PaymentResultWithDataListener
 import dagger.hilt.android.AndroidEntryPoint
 import `in`.furniture.furnishar.screens.DetailScreen
 import `in`.furniture.furnishar.screens.HomeScreen
 import `in`.furniture.furnishar.screens.SplashScreen
 import `in`.furniture.furnishar.ui.theme.FurnishARTheme
+import org.json.JSONObject
 
 @AndroidEntryPoint
-class MainActivity : ComponentActivity() {
+class MainActivity : ComponentActivity(), PaymentResultWithDataListener, ExternalWalletListener {
+
+    private lateinit var checkout: Checkout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Checkout.preload(applicationContext)
+
+        checkout = Checkout()
+        checkout.setKeyID(BuildConfig.RAZORPAY_KEY)
+
+        val options = JSONObject()
+        options.apply {
+            put("name", "Razorpay Corp")
+            put("description", "Demoing Charges")
+            put("image", "https://firebasestorage.googleapis.com/v0/b/furnishar-ar.appspot.com/o/ic_launcher-playstore.jpg?alt=media&token=12decb7f-02d8-4fb1-aec1-1951fc61d6ec")
+            put("theme.color", "#8A49F7")
+            put("currency", "INR")
+            put("amount", "20000")
+        }
+
+        val retryObj = JSONObject()
+        retryObj.put("enabled", true)
+        retryObj.put("max_count", 4)
+
+        options.put("retry", retryObj)
+
+        val prefill = JSONObject()
+        prefill.put("contact", "9118882517")
+
+        options.put("prefill", prefill)
+
+        checkout.open(this, options)
+
         setContent {
             FurnishARTheme {
                 Surface(
@@ -41,5 +78,17 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+    }
+
+    override fun onPaymentSuccess(razorpayPaymentId: String?, paymentData: PaymentData?) {
+        Toast.makeText(this, "Thanks for shopping with FurnishAR", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onPaymentError(p0: Int, p1: String?, p2: PaymentData?) {
+        Toast.makeText(this, p0.toString(), Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onExternalWalletSelected(p0: String?, p1: PaymentData?) {
+        Toast.makeText(this, p0, Toast.LENGTH_SHORT).show()
     }
 }
